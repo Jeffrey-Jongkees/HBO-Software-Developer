6afc4eaaf2a2d3b328f9f80b12b42e0d191542e9
Jeffrey Jongkees <jeffreyjongkees@bsure-digital.nl>
2024-05-01 10:31:31 +0200

    Co-authored-by: Udder <PirateEra@users.noreply.github.com>

---
+++ eindopdracht/assets/javascript/maps.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+let map;
+window.addEventListener("load", (event) => {
+  initMap();
+});
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 52.063796, lng: 5.104179 },
+    zoom: 17,
+    mapId: "4504f8b37365c3d0"
+  });
+  const markerViewWithText = new AdvancedMarkerElement({
+    map,
+    position: { lat: 52.063796, lng: 5.104179 },
+    title: "Dit is de dans school locatie",
+  });
+  // Adjust the scale.
+  const pinScaled = new PinElement({
+    scale: 1.5,
+  });
+}
